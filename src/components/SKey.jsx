0cb89a13ef2946e48c479517931c67bd222656d3
Akinfenwa Taiwo Christopher <101477417+AChris-T@users.noreply.github.com>
2025-06-05 18:33:19 +0100

    feat:added the home page

--- src/components/SKey.jsx
+++ src/components/SKey.jsx
@@ -7,7 +7,7 @@ export default function SKey() {
       <h3 className="text-[#708238] font-bold text-2xl">
         Key Problems Addressed
       </h3>
-      <div className="flex gap-10">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
         <div className="flex flex-5 border-[#E5EBD1] border-[1px] justify-center py-2 rounded-xl items-center text-lg text-[#121212]">
           <User /> High Turnover Rates
         </div>
